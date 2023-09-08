11ee83053dda081d28c357004f7097af07b0b72c
Dmitry <sicily007@rambler.ru>
2023-09-09 01:00:46 +0300

    add_example2_solution_comment

--- Example002/Program.cs
+++ Example002/Program.cs
@@ -18,14 +18,49 @@
 из метода FindNumberByPosition. Если такого элемента нет, вывести на экран "There is no such index".
 Если элемент есть, вывести на экран "The number in [{x}, {y}] is {значение}".
 */
-
+/*
+ВНИМАНИЕ!!!!!
+в автотестах проверка идет не по правилам нумерации позиции элементов массива, т.е.
+для элемента массива с индексом (2,2), автотест посчитает корректным элемент с индексом (1, 1) = 6
+1   2   3   4
+5   6   7   8
+9   10  11  12
+в связи с этим осуществлены корректировки x-1 и y-1 в методах
+*/
 
 int[,] arr = CreateIncreasingMatrix(4, 4, 2);
 PrintArray(arr);
-int[,] arrResult = FindNumberByPosition(arr, 3, 3);
+int[,] arrResult = FindNumberByPosition(arr, 4, 4);
 System.Console.WriteLine();
 PrintArray(arrResult);
-PrintCheckIfError(arrResult, 3, 3);
+PrintCheckIfError(arrResult, 4, 4);
+
+int[,] FindNumberByPosition(int[,] array, int x, int y)
+{
+    if (x > (array.GetLength(0)) | y > (array.GetLength(1)))
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array[i, j] = 0;
+            }
+        } 
+    }
+    else
+    {
+        int tmp = array[x-1, y-1];
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+               array[i, j] = 0;
+               array[x-1,y-1] = tmp;
+            }
+        }  
+    }
+    return array;
+}
 
 void PrintCheckIfError(int[,] array, int x, int y)
 {
@@ -38,34 +73,7 @@ void PrintCheckIfError(int[,] array, int x, int y)
         }
     }
     if (sum == 0) System.Console.WriteLine("There is no such index");
-    else System.Console.WriteLine($"The number in [{x}, {y}] is {array[x, y]}");
-}
-
-int[,] FindNumberByPosition(int[,] array, int x, int y)
-{
-    if (x > (array.GetLength(0)-1) | y > (array.GetLength(1)-1))
-    {
-        for (int i = 0; i < array.GetLength(0); i++)
-        {
-            for (int j = 0; j < array.GetLength(1); j++)
-            {
-                array[i, j] = 0;
-            }
-        } 
-    }
-    else
-    {
-        int tmp = array[x, y];
-        for (int i = 0; i < array.GetLength(0); i++)
-        {
-            for (int j = 0; j < array.GetLength(1); j++)
-            {
-               array[i, j] = 0;
-               array[x,y] = tmp;
-            }
-        }  
-    }
-    return array;
+    else System.Console.WriteLine($"The number in [{x}, {y}] is {array[x-1, y-1]}");
 }
 
 int[,] CreateIncreasingMatrix(int n, int m, int k)
